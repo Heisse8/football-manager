75f72fb551865ea0559883430ccce63e82c4bb15
Thomas Heissenhuber <thomasheissenhuber@iMac-von-Thomas-2.fritz.box>
2026-02-22 15:03:46 +0100

    Fix render port issue

--- game/index.js
+++ game/index.js
@@ -59,7 +59,7 @@ app.get("/", (req, res) => {
    SERVER START
 ========================= */
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server läuft auf Port ${PORT}`);
