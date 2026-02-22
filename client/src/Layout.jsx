514adb3ea99f71ab1f3b721db82b46ad53537bf4
Thomas Heissenhuber <thomasheissenhuber@iMac-von-Thomas-2.fritz.box>
2026-02-22 23:08:24 +0100

    Update Register layout

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,11 +1,26 @@
-import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import bgImage from "./assets/manager-office.jpg";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900 text-white">
-      <Navbar />
-      <Outlet />
+    <div className="relative min-h-screen text-white overflow-hidden">
+
+      {/* Hintergrundbild */}
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-fixed scale-105"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      />
+
+      {/* Dunkler Overlay */}
+      <div className="absolute inset-0 bg-gradient-to-br from-black/80 via-black/70 to-black/90" />
+
+      {/* Inhalt */}
+      <div className="relative z-10">
+        <Navbar />
+        <Outlet />
+      </div>
+
     </div>
   );
 }
