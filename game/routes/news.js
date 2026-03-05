472c417f785dc35cd9ebeafccc043fa8e2b7e59e
Thomas Heissenhuber <thomasheissenhuber@iMac-von-Thomas-2.fritz.box>
2026-03-05 20:32:16 +0100

    fix news route

--- game/routes/news.js
+++ game/routes/news.js
@@ -1,77 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
 const News = require("../models/News");
-const Team = require("../models/Team");
 
-async function generateMatchNews(match){
+/* =========================================
+ NEWS LADEN
+========================================= */
 
-const homeTeam = match.homeTeam.name;
-const awayTeam = match.awayTeam.name;
+router.get("/", async (req,res)=>{
 
-const homeGoals = match.homeGoals;
-const awayGoals = match.awayGoals;
+try{
 
-let title;
-let content;
+const news = await News.find()
+.sort({ createdAt:-1 })
+.limit(50);
 
-if(homeGoals > awayGoals){
+res.json(news);
 
-title = `${homeTeam} besiegt ${awayTeam}`;
+}catch(err){
 
-content = `${homeTeam} gewinnt mit ${homeGoals}:${awayGoals} gegen ${awayTeam}.`;
+console.error(err);
 
-}
-else if(awayGoals > homeGoals){
-
-title = `${awayTeam} gewinnt bei ${homeTeam}`;
-
-content = `${awayTeam} setzt sich mit ${awayGoals}:${homeGoals} durch.`;
+res.status(500).json({message:"Serverfehler"});
 
 }
-else{
-
-title = `Remis zwischen ${homeTeam} und ${awayTeam}`;
-
-content = `${homeTeam} und ${awayTeam} trennen sich ${homeGoals}:${awayGoals}.`;
-
-}
-
-await News.create({
-
-title,
-content,
-type:"match",
-league:match.league
 
 });
 
-}
-
-async function generateTeamCrisisNews(){
-
-const teams = await Team.find();
-
-for(const team of teams){
-
-if(team.losses >= 3){
-
-await News.create({
-
-title:`Trainer bei ${team.name} unter Druck`,
-
-content:`${team.name} hat mehrere Niederlagen in Folge kassiert. Steht der Trainer vor dem Aus?`,
-
-type:"team",
-
-team:team._id
-
-});
-
-}
-
-}
-
-}
-
-module.exports = {
-generateMatchNews,
-generateTeamCrisisNews
-};
+module.exports = router;
