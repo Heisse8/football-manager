a602788e1f05cbfe831eaab4fbb3b7675a9f953c
Thomas Heissenhuber <thomasheissenhuber@iMac-von-Thomas-2.fritz.box>
2026-03-15 20:16:24 +0100

    fix replaceBotTeam crash when no bot exists

--- game/utils/replaceBotTeam.js
+++ game/utils/replaceBotTeam.js
@@ -6,40 +6,21 @@ if(!userTeam.owner){
 throw new Error("User Team hat keinen Besitzer");
 }
 
-/* schwächsten Bot suchen */
-
-const botTeam = await Team.findOne({
+const bot = await Team.findOne({
 league: userTeam.league,
-isBot:true
-}).sort({ fanBase:1 });
+isBot: true
+});
 
-if(!botTeam){
+if(!bot){
 console.log("Kein Bot Team verfügbar");
-return;
+return;   // <- wichtig: nicht crashen
 }
 
-/* Bot → User */
+/* Bot löschen */
 
-botTeam.owner = userTeam.owner;
+await Team.deleteOne({ _id: bot._id });
 
-botTeam.name = userTeam.name;
-botTeam.shortName = userTeam.shortName;
-
-botTeam.clubIdentity = userTeam.clubIdentity;
-
-botTeam.balance = userTeam.balance;
-botTeam.fanBase = userTeam.fanBase;
-botTeam.homeBonus = userTeam.homeBonus;
-
-botTeam.isBot = false;
-
-await botTeam.save();
-
-/* temporäres Team löschen */
-
-await Team.deleteOne({ _id:userTeam._id });
-
-console.log("Bot Team ersetzt:", botTeam.name);
+console.log(`Bot Team ${bot.name} ersetzt`);
 
 }
 
