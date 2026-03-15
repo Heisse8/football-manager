c4763645660482be8f6f56afcf22475d5d342741
Thomas Heissenhuber <thomasheissenhuber@iMac-von-Thomas-2.fritz.box>
2026-03-15 17:28:40 +0100

    fix mongoose connection options

--- game/server.js
+++ game/server.js
@@ -139,10 +139,7 @@ message:"Interner Serverfehler"
 DATABASE CONNECTION
 ===================================================== */
 
-mongoose.connect(process.env.MONGO_URI,{
-useNewUrlParser:true,
-useUnifiedTopology:true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
 
 console.log("MongoDB verbunden");
